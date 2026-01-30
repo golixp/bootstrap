1a3319de2abef21d79e513b1a61b36d692a4931d
lixp <golixp@outlook.com>
2026-01-30 19:34:46 +0800

    增加单独的Gurb 安装Snapper脚本

--- arch-linux/install-snapper-grub.sh
+++ arch-linux/install-snapper-grub.sh
@@ -10,7 +10,11 @@ echo "安装 snapper 相关软件..."
 sudo pacman -S --needed --noconfirm \
   extra/snapper \
   extra/btrfs-assistant \
+  extra/grub-btrfs \
   extra/snap-pac
+
+echo "更新 Grub 配置..."
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # 安装成功后提示
 echo "所有软件包已成功安装!"
